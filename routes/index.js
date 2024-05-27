4e0cfe3124a5ea49aa08a8aaec656e72c197262d
kilyion <kilyion.romary@viacesi.fr>
2024-05-27 16:09:10 +0200

    feature(ajout connexion API test)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+let deliveries = [
+  {
+    id: 1,
+    from: 'Location A',
+    to: 'Location B',
+    orderNumber: '12345',
+    price: 20
+  },
+  {
+    id: 2,
+    from: 'Location C',
+    to: 'Location D',
+    orderNumber: '67890',
+    price: 25
+  },
+  {
+    id: 3,
+    from: 'Location A',
+    to: 'Location B',
+    orderNumber: '12345',
+    price: 500
+  },
+  {
+    id: 4,
+    from: 'Location C',
+    to: 'Location D',
+    orderNumber: '67890',
+    price: 2500
+  },
+  {
+    id: 5,
+    from: 'Location A',
+    to: 'Location B',
+    orderNumber: '12345',
+    price: 2000
+  },
+  {
+    id: 6,
+    from: 'Location C',
+    to: 'Location D',
+    orderNumber: '67890',
+    price: 25888
+  }
+];
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
+});
+
+// Route to get deliveries
+router.get('/deliveries', function(req, res, next) {
+  res.json(deliveries);
 });
 
 module.exports = router;
